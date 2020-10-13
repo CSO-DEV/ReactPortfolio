ced6a1eff1509cd62d55e50fab21d2f9d86c446a
Carole SOTIRIO <ca.sotirio@gmail.com>
2020-10-13 13:14:28 +0200

    Updates

---
+++ precache-manifest.73522d3a19c8247d433202549cb76651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353a8e70a28f85444ee2d424ed9d2163",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "6043c3b781a558659009",
+    "url": "/ReactPortfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1d81ce069c26f4942fa0",
+    "url": "/ReactPortfolio/static/css/main.b5de9812.chunk.css"
+  },
+  {
+    "revision": "6043c3b781a558659009",
+    "url": "/ReactPortfolio/static/js/2.2e39ed35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactPortfolio/static/js/2.2e39ed35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d81ce069c26f4942fa0",
+    "url": "/ReactPortfolio/static/js/main.3c60d17e.chunk.js"
+  },
+  {
+    "revision": "245e88873ef0a2539a5e",
+    "url": "/ReactPortfolio/static/js/runtime-main.ebaa2495.js"
+  }
+]);
