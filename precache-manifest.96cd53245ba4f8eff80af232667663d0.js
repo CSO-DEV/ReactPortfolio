96bcece5f2c6a288a51b3fcf48e3e8dcbdfc9ebb
Carole SOTIRIO <ca.sotirio@gmail.com>
2020-10-13 14:48:16 +0200

    Updates

---
+++ precache-manifest.96cd53245ba4f8eff80af232667663d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3b590c8256fcaa89e327255a57177c",
+    "url": "/PortFolio/index.html"
+  },
+  {
+    "revision": "6043c3b781a558659009",
+    "url": "/PortFolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1bc3ba6eddbfb8403ffa",
+    "url": "/PortFolio/static/css/main.b5de9812.chunk.css"
+  },
+  {
+    "revision": "6043c3b781a558659009",
+    "url": "/PortFolio/static/js/2.2e39ed35.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PortFolio/static/js/2.2e39ed35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc3ba6eddbfb8403ffa",
+    "url": "/PortFolio/static/js/main.8e491609.chunk.js"
+  },
+  {
+    "revision": "7f77e2718b4c22a95589",
+    "url": "/PortFolio/static/js/runtime-main.32d61960.js"
+  }
+]);
