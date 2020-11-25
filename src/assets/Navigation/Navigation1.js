a5c8fdeebe16c9528f66b0b1e8cf2432c8e55088
Carole SOTIRIO <ca.sotirio@gmail.com>
2020-11-25 21:35:26 +0100

    navbarre

--- src/assets/Navigation/Navigation1.js
+++ src/assets/Navigation/Navigation1.js
@@ -1,13 +1,120 @@
 import React from 'react';
-import "./style.scss";
+import "./style1.scss";
 import data from "../../configData/dataPortfolio.json";
 import config from "../../configData/config.json";
 
 
+
 function Navigation1(props) {
+  /**
+   * @var shortData : Données extraites BDD * DB extracted data
+   */
+  let shortData = data.propos.contenuSection[0];
+
+  /**
+   * @method startNavigation 
+   */
+    const startNavigation=()=>{
+       
+    }
+
+  /**
+   * @method fixedNavigationBar 
+   */
+  const fixedNavigationBar=()=>{
+       
+    }  
+    
+  /**
+   * @method topNavigationBar 
+   */
+  const topNavigationBar=()=>{
+      return(
+          <div className="nav">
+              <div className="topNavigationBarTitle"
+              style={{
+                  display:"flex",
+              }}>
+                <div className="topNavigationBarImg"
+                style={{
+                    margin:"5px",
+                    width: "77px",
+                }}>
+                    <img 
+                    src={config.img + shortData.photoPropos} alt={"Photo profil " + shortData.nomPropos}
+                    style={{
+                        borderRadius:"50%",
+                        width: "100%",
+                    }}/>
+                </div>
+                <div className="topNavigationBarText"
+                style={{
+                    margin:"5px",
+                    position: "relative",
+                    width: "400px",
+                    display: "flex",
+                    alignItems: "center",
+                }}>
+                    <div className="topNavigationBarH1"
+                    style={{
+                      
+                    }}>
+                        <h1 
+                    style={{
+                        margin:"0",
+                        fontSize:"17px",
+                        }}>{shortData.nomPropos}</h1>
+                    <h1 
+                    style={{
+                        margin:"0",
+                        fontSize:"27px",
+                        fontWeight:"bolder",
+                        textTransform: "uppercase",
+                        }}>{shortData.titrePropos}</h1>
+                    </div>
+                </div>
+              </div>
+              <input type="checkbox" id="nav-check"/>
+              <div className="nav-btn">
+                  <label for="nav-check">
+                    <img src={config.img + "burger.png"} className="burger"/>
+                  </label>
+              </div>
+              <div className="nav-links">
+                  <label 
+                  for="nav-check" 
+                  id="propos" 
+                  className="navItems">A propos..</label>
+                  <label 
+                  for="nav-check" 
+                  id="formations" 
+                  className="navItems">Mes formations</label>    
+                  <label 
+                  for="nav-check" 
+                  id="technologies" 
+                  className="navItems">Mes technologies</label>
+                  <label 
+                  for="nav-check" 
+                  id="portfolio"
+                  className="navItems">Mon portfolio</label>            
+
+              </div>
+
+          </div>
+      )
+       
+    }  
     return (
         <div>
-            test
+            <div className="startNavigation">
+            {startNavigation()}
+            </div>
+            <div className="fixedNavigationBar">
+            {fixedNavigationBar()}
+            </div>
+            <div className="topNavigationBar">
+            {topNavigationBar()}
+            </div>
         </div>
     );
 }
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -14,39 +14,7 @@ import "./style.scss";
 
 function Homepage(props) {
 
-  /**
-   * @method startDisplay : Affichage au démarrage de la page * Start page display
-   * @var sectionList : Tableau sections BDD * DB sections array                     
-   */
-  const startDisplay=()=>{
-    setTimeout(function(){
-      document.getElementById("depart").style.display = "none";     
-      document.getElementById("propos").style.display = "flex";
-      let sectionList=Object.keys(data);
-      sectionList.forEach((element,index)=>{
-        if (data[element].lienSection!="contact"){
-          document.getElementById(data[element].lienSection).style.display = "flex";
-        }
-        
-      });
-      }
-      , 10000);
-    /*setTimeout(function(){      
-      document.getElementsByClassName("listeNavigation")[0].style.opacity = "1";   
-      document.getElementsByClassName("boutonNavigation")[0].style.opacity = "1"; 
-      }
-      , 15000);*/
-    }  
- 
- /**
-  * @method imageHomepage : Affichage de l'image de fond * Background image display
-  */
-  const imageHomepage=()=>{
-    return (
-      <div id="depart" className="tousLiens imageFond all" style={{"height":"100vh","width":"100vw", "overflow":"hidden"}}>
-        <img src={config.img + "webDev.jpg"} style={{"height": "100%", "width":"100%"}} alt="image Geek :)"/>
-      </div>)    
-  }
+
 
   /**
    * @render Construction du DOM * DOM build
@@ -54,8 +22,6 @@ function Homepage(props) {
   return (
     <div className="homepage" id="homepage">
       <Navigation1 />
-      {imageHomepage()}
-      {startDisplay()}
       <Presentation/>
       <Formation/>
       <Technologie/>
